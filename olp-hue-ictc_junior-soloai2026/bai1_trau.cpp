c1f78b505c016f016d0d1e5e3b07ccce67e21d95
Nourist <hodinhvys@gmail.com>
2026-07-07 00:12:54 +0700

    Update contest: olp-hue-ictc_junior-soloai2026

---
+++ olp-hue-ictc_junior-soloai2026/bai1_trau.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i,l,r) for (int i = l; i <= r; i++)
+#define FOD(i,r,l) for (int i = r; i >= l; i--)
+#define ll long long
+#define ld long double
+#define X first
+#define Y second
+#define Name "bai1"
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	if (fopen(Name".inp", "r")) {
+		freopen(Name".inp", "r", stdin);
+		freopen(Name".ans", "w", stdout);
+	}
+
+	int n, k;
+	cin >> n >> k;
+
+	int res = 0;
+	FOR(i, 1, n)FOR(j, 1, n)if ((i + j) % k == 0)res++;
+	cout << res;
+}
