0506bd92f8c35aa4f3a5ba39d685cea500c7a23f
Nourist <hodinhvys@gmail.com>
2026-07-06 23:37:45 +0700

    Update contest: dexuat-dhbb-binhdinh2022

---
+++ dexuat-dhbb-binhdinh2022/TAMGIAC.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i,l,r) for (int i = l; i <= r; ++i)
+#define FOD(i,r,l) for (int i = r; i >= l; --i)
+#define ll long long
+#define ld long double
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	if(fopen("TAMGIAC.inp", "r")) {
+		freopen("TAMGIAC.inp", "r", stdin);
+		freopen("TAMGIAC.out", "w", stdout);
+	}
+
+	
+}
