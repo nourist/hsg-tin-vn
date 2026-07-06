0ddb0b9d2e8a709cec0f1ca5d74a29aa69822e8a
Nourist <hodinhvys@gmail.com>
2026-07-07 00:13:40 +0700

    Update contest: ts10_vinhlong2023-24

---
+++ ts10_vinhlong2023-24/bai1.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	if(fopen("bai1.inp", "r")) {
+		freopen("bai1.inp", "r", stdin);
+		freopen("bai1.out", "w", stdout);
+	}
+
+	ld x, y;
+	cin >> x;
+
+	y = x / 3 * 2;
+	cout << fixed << setprecision(2) << x * y;
+}
