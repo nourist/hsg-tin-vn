39e0b26b5cf0510814dd5e33b14633f940eabddc
Nourist <hodinhvys@gmail.com>
2026-07-07 00:13:05 +0700

    Update contest: thcs_hanoi2016-17

---
+++ thcs_hanoi2016-17/cau3.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	if(fopen("cau3.inp", "r")) {
+		freopen("cau3.inp", "r", stdin);
+		freopen("cau3.out", "w", stdout);
+	}
+
+	
+}
