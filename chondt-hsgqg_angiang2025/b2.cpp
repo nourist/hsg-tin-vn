b9d9bbe776e3e81d2c80b83c6180bfa2c2a74b5b
Nourist <hodinhvys@gmail.com>
2026-08-01 07:29:16 +0700

    Update contest: chondt-hsgqg_angiang2025

--- chondt-hsgqg_angiang2025/b2.cpp
+++ chondt-hsgqg_angiang2025/b2.cpp
@@ -21,6 +21,36 @@ using namespace std;
 #define TURN(x, i) ((x) ^ (1LL << (i)))
 #define Name "b2"
 
+int n, A, B, a[10], b[10];
+const ll mod = 1e9 + 7;
+ll dp[35][55][55];
+
+int f(int idx, int carryA, int carryB) {
+	if (idx == 31)return carryA == 0 && carryB == 0;
+
+	ll& memo = dp[idx][carryA][carryB];
+	if (memo != -1)return memo;
+
+	memo = 0;
+
+	FOR(mask, 0, (1 << n) - 1) {
+		int sumA = carryA;
+		int sumB = carryB;
+
+		FOR(i, 0, n - 1) {
+			if (BIT(mask, i)) {
+				sumA += a[i];
+				sumB += b[i];
+			}
+		}
+
+		if (sumA % 2 == BIT(A, idx) && sumB % 2 == BIT(B, idx))
+			memo += f(idx + 1, sumA / 2, sumB / 2);
+		memo %= mod;
+	}
+	return memo;
+}
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -31,5 +61,10 @@ int main() {
 		freopen(Name".out", "w", stdout);
 	}
 
-	
+	memset(dp, -1, sizeof dp);
+
+	cin >> n >> A >> B;
+	FOR(i, 0, n - 1)cin >> a[i] >> b[i];
+
+	cout << f(0, 0, 0);
 }
