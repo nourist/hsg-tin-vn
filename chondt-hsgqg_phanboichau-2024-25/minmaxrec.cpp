2f5dd15b7a68870334a6b1a485880025f6ce1202
Nourist <hodinhvys@gmail.com>
2026-08-10 14:22:02 +0700

    Update contest: chondt-hsgqg_phanboichau-2024-25

--- chondt-hsgqg_phanboichau-2024-25/minmaxrec.cpp
+++ chondt-hsgqg_phanboichau-2024-25/minmaxrec.cpp
@@ -1,52 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define FOR(i,l,r) for (int i = l; i <= r; ++i)
-#define FOD(i,r,l) for (int i = r; i >= l; --i)
+#define FOR(i, l, r) for (int i = l; i <= r; ++i)
+#define FOD(i, r, l) for (int i = r; i >= l; --i)
 #define ll long long
 #define ld long double
-#define pii pair<int,int>
-#define X first 
+#define pii pair<int, int>
+#define X first
 #define Y second
 
 int a[2004][2004];
 
 pii maxPair(pii x, int u) {
-	if (u <= x.X) {
-		x.Y = x.X;
-		x.X = u;
-	}
-	else if (u <= x.Y) {
-		x.Y = u;
-	}
-	return x;
+  if (u <= x.X) {
+    x.Y = x.X;
+    x.X = u;
+  } else if (u <= x.Y) {
+    x.Y = u;
+  }
+  return x;
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
-	if (fopen("minmaxrec.inp", "r")) {
-		freopen("minmaxrec.inp", "r", stdin);
-		freopen("minmaxrec.out", "w", stdout);
-	}
+  if (fopen("minmaxrec.inp", "r")) {
+    freopen("minmaxrec.inp", "r", stdin);
+    freopen("minmaxrec.out", "w", stdout);
+  }
 
-	int n, m;
-	cin >> n >> m;
+  int n, m;
+  cin >> n >> m;
 
-	if (n > m)swap(n, m);
+  if (n > m)
+    swap(n, m);
 
-	FOR(i, 1, n)FOR(j, 1, m)cin >> a[i][j];
+  FOR(i, 1, n) FOR(j, 1, m) cin >> a[i][j];
 
-	int res = 1e9;
+  int res = 1e9;
 
-	FOR(i, 1, n)FOR(j, i + 1, n) {
-		pii tmp = { 1e9,1e9 };
-		FOR(k, 1, m) {
-			tmp = maxPair(tmp, max(a[i][k], a[j][k]));
-		}
-		res = min(res, max(tmp.X, tmp.Y));
-	}
-	cout << res;
+  FOR(i, 1, n) FOR(j, i + 1, n) {
+    pii tmp = {1e9, 1e9};
+    FOR(k, 1, m) { tmp = maxPair(tmp, max(a[i][k], a[j][k])); }
+    res = min(res, max(tmp.X, tmp.Y));
+  }
+  cout << res;
 }
