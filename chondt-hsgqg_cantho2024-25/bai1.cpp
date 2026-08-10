b6c198bdf032840691c63e75c2c0f493ab7cdba3
Nourist <hodinhvys@gmail.com>
2026-08-10 14:22:02 +0700

    Update contest: chondt-hsgqg_cantho2024-25

--- chondt-hsgqg_cantho2024-25/bai1.cpp
+++ chondt-hsgqg_cantho2024-25/bai1.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-#define FOR(i,l,r) for (int i = l; i <= r; ++i)
-#define FOD(i,r,l) for (int i = r; i >= l; --i)
+#define FOR(i, l, r) for (int i = l; i <= r; ++i)
+#define FOD(i, r, l) for (int i = r; i >= l; --i)
 #define ll long long
 #define ld long double
 
@@ -12,34 +13,31 @@ int q[(int)1e5 + 5];
 int h[(int)1e5 + 5];
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	if (fopen("bai1.inp", "r")) {
-		freopen("bai1.inp", "r", stdin);
-		freopen("bai1.out", "w", stdout);
-	}
+    if (fopen("bai1.inp", "r")) {
+        freopen("bai1.inp", "r", stdin);
+        freopen("bai1.out", "w", stdout);
+    }
+    int n, k;
+    cin >> n >> k;
 
-	int n, k;
-	cin >> n >> k;
+    FOR(i, 1, n) cin >> a[i];
 
-	FOR(i, 1, n)cin >> a[i];
+    FOR(i, 1, n) q[i] = -a[i];
+    FOR(i, 2, n) q[i] = max(q[i], q[i - 1]);
 
-	FOR(i, 1, n)q[i] = -a[i];
-	FOR(i, 2, n)q[i] = max(q[i], q[i - 1]);
+    FOR(t, 1, k) {
+        FOR(i, 2, n) {
+            dp[i] = max(dp[i], q[i - 1] + a[i]);
+            h[i] = max(h[i - 1], dp[i]);
+        }
+        FOR(i, 2, n) { q[i] = max(q[i - 1], h[i - 1] - a[i]); }
+    }
 
-	FOR(t, 1, k) {
-		FOR(i, 2, n) {
-			dp[i] = max(dp[i], q[i - 1] + a[i]);
-			h[i] = max(h[i - 1], dp[i]);
-		}
-		FOR(i, 2, n) {
-			q[i] = max(q[i - 1], h[i - 1] - a[i]);
-		}
-	}
-
-	cout << *max_element(dp + 1, dp + n + 1);
+    cout << *max_element(dp + 1, dp + n + 1);
 }
 
 /*
--- chondt-hsgqg_cantho2024-25/bai3.cpp
+++ chondt-hsgqg_cantho2024-25/bai3.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define FOR(i,l,r) for (int i = l; i <= r; ++i)
-#define FOD(i,r,l) for (int i = r; i >= l; --i)
+#define FOR(i, l, r) for (int i = l; i <= r; ++i)
+#define FOD(i, r, l) for (int i = r; i >= l; --i)
 #define ll long long
 #define ld long double
 #define vi vector<int>
@@ -15,50 +15,52 @@ int res[(int)1e5 + 5];
 int timeDfs = 1;
 
 void dfs(int u, int par) {
-	num[u] = low[u] = timeDfs++;
+    num[u] = low[u] = timeDfs++;
 
-	int cnt1 = 0;
-	int cnt2 = 0;
+    int cnt1 = 0;
+    int cnt2 = 0;
 
-	for (auto v : adj[u]) {
-		if (v == par)continue;
+    for (auto v : adj[u]) {
+        if (v == par)
+            continue;
 
-		if (!num[v]) {
-			dfs(v, u);
-			low[u] = min(low[u], low[v]);
-			cnt1++;
-			cnt2 += low[v] >= num[u];
-		}
-		else {
-			low[u] = min(low[u], num[v]);
-		}
-	}
+        if (!num[v]) {
+            dfs(v, u);
+            low[u] = min(low[u], low[v]);
+            cnt1++;
+            cnt2 += low[v] >= num[u];
+        } else {
+            low[u] = min(low[u], num[v]);
+        }
+    }
 
-	if (par == 0)res[u] = cnt1;
-	else res[u] = cnt2 + 1;
+    if (par == 0)
+        res[u] = cnt1;
+    else
+        res[u] = cnt2 + 1;
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	if (fopen("bai3.inp", "r")) {
-		freopen("bai3.inp", "r", stdin);
-		freopen("bai3.out", "w", stdout);
-	}
+    if (fopen("bai3.inp", "r")) {
+        freopen("bai3.inp", "r", stdin);
+        freopen("bai3.out", "w", stdout);
+    }
 
-	int n, m;
-	cin >> n >> m;
+    int n, m;
+    cin >> n >> m;
 
-	FOR(i, 1, m) {
-		int u, v;
-		cin >> u >> v;
-		adj[u].insert(v);
-		adj[v].insert(u);
-	}
+    FOR(i, 1, m) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].insert(v);
+        adj[v].insert(u);
+    }
 
-	dfs(1, 0);
+    dfs(1, 0);
 
-	FOR(i, 1, n)cout << res[i] << ' ';
+    FOR(i, 1, n) cout << res[i] << ' ';
 }
