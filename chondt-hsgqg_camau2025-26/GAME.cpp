69ee6f3dd256dd9edd306a88171348b01ea74cb5
Nourist <hodinhvys@gmail.com>
2026-08-17 21:01:53 +0700

    Update contest: chondt-hsgqg_camau2025-26

--- chondt-hsgqg_camau2025-26/GAME.cpp
+++ chondt-hsgqg_camau2025-26/GAME.cpp
@@ -1,20 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define FOR(i,l,r) for (int i = l; i <= r; ++i)
-#define FOD(i,r,l) for (int i = r; i >= l; --i)
+#define FOR(i,l,r) for (int i = (l); i <= (r); ++i)
+#define FOD(i,r,l) for (int i = (r); i >= (l); --i)
 #define ll long long
-#define ld long double
+#define db double
+#define X first
+#define Y second
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vl vector<ll>
+#define vii vector<pii>
+#define vll vector<pll>
+#define pb push_back
+#define all(v) (v).begin(), (v).end()
+#define endl '\n'
+#define BIT(x, i) (((x) >> (i)) & 1)
+#define BIT_COUNT(x) __builtin_popcountll(x)
+#define LOWEST_BIT(x) __builtin_ctzll(x)
+#define MASK(i) (1LL << (i))
+#define TURNON(x, i) ((x) | (1LL << (i)))
+#define TURNOFF(x, i) ((x) & ~(1LL << (i)))
+#define TURN(x, i) ((x) ^ (1LL << (i)))
+#define Name "game"
+
+mt19937_64 Rand(chrono::steady_clock::now().time_since_epoch().count());
+
+int u[(int)1e5 + 5];
+int v[(int)1e5 + 5];
+int s[(int)1e5 + 5];
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	if(fopen("GAME.inp", "r")) {
-		freopen("GAME.inp", "r", stdin);
-		freopen("GAME.out", "w", stdout);
+	if (fopen(Name".inp", "r")) {
+		freopen(Name".inp", "r", stdin);
+		freopen(Name".out", "w", stdout);
 	}
 
-	
+	int n, m, d, k;
+	cin >> n >> m >> d >> k;
+
+	vector<vi> a(n + 2, vi(m + 2, -1));
+	vector<vi> D(n + 2, vi(m + 2, 0));
+
+	FOR(i, 1, k) {
+		cin >> u[i] >> v[i] >> s[i];
+		a[u[i]][v[i]] = s[i];
+	}
+
+	FOR(i, 1, n)FOR(j, 1, m) {
+		if (a[i][j] == -1) {
+			continue;
+		}
+
+		D[i][j] = (a[i][j] - D[i - 1][j] + d) % d;
+	}
+
+	FOR(i, 1, k) {
+		cout << 0 << ' ' << D[u[i] - 1][v[i]] << ' ' << 0 << ' ' << D[u[i]][v[i]] << endl;
+	}
 }
--- chondt-hsgqg_camau2025-26/TOUR.cpp
+++ chondt-hsgqg_camau2025-26/TOUR.cpp
@@ -1,20 +1,85 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define FOR(i,l,r) for (int i = l; i <= r; ++i)
-#define FOD(i,r,l) for (int i = r; i >= l; --i)
+#define FOR(i,l,r) for (int i = (l); i <= (r); ++i)
+#define FOD(i,r,l) for (int i = (r); i >= (l); --i)
 #define ll long long
-#define ld long double
+#define db double
+#define X first
+#define Y second
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vl vector<ll>
+#define vii vector<pii>
+#define vll vector<pll>
+#define pb push_back
+#define all(v) (v).begin(), (v).end()
+#define endl '\n'
+#define BIT(x, i) (((x) >> (i)) & 1)
+#define BIT_COUNT(x) __builtin_popcountll(x)
+#define LOWEST_BIT(x) __builtin_ctzll(x)
+#define MASK(i) (1LL << (i))
+#define TURNON(x, i) ((x) | (1LL << (i)))
+#define TURNOFF(x, i) ((x) & ~(1LL << (i)))
+#define TURN(x, i) ((x) ^ (1LL << (i)))
+#define Name "tour"
+
+mt19937_64 Rand(chrono::steady_clock::now().time_since_epoch().count());
+
+ll dp[(int)2e5 + 5];
+int s[(int)2e5 + 5];
+bool p[(int)2e5 + 5];
+vi adj[(int)2e5 + 5];
+
+ll res = -1e18;
+
+void dfs(int u, int par) {
+	dp[u] = p[u] ? s[u] : -1e18;
+	vl t;
+	for (int v : adj[u]) {
+		if (v == par)continue;
+
+		dfs(v, u);
+		dp[u] = max(dp[u], dp[v] + s[u]);
+		t.pb(dp[v]);
+	}
+
+	sort(all(t));
+	if (t.size() >= 2) {
+		res = max(res, t.back() + t[t.size() - 2] + s[u]);
+	}
+
+	if (t.size() >= 1)
+		if (p[u])res = max(res, s[u] + t.back());
+
+	if (p[u])res = max(res, 1LL * s[u]);
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	if(fopen("TOUR.inp", "r")) {
-		freopen("TOUR.inp", "r", stdin);
-		freopen("TOUR.out", "w", stdout);
+	if (fopen(Name".inp", "r")) {
+		freopen(Name".inp", "r", stdin);
+		freopen(Name".out", "w", stdout);
 	}
 
-	
+	memset(p, 1, sizeof p);
+	p[0] = p[1] = 0;
+	for (int i = 2; i * i <= 2e5; i++)if (p[i])for (int j = i * i; j <= 2e5; j += i)p[j] = 0;
+
+	int n;
+	cin >> n;
+	FOR(i, 1, n)cin >> s[i];
+	FOR(i, 1, n - 1) {
+		int u, v;
+		cin >> u >> v;
+		adj[u].pb(v);
+		adj[v].pb(u);
+	}
+	dfs(1, 0);
+	cout << res;
 }
+
