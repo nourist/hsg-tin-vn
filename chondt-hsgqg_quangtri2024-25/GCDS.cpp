e6c38b5d7fcb5450d22ce5c73f588d4a5ce5ff5d
Nourist <hodinhvys@gmail.com>
2026-08-17 21:01:54 +0700

    Update contest: chondt-hsgqg_quangtri2024-25

--- chondt-hsgqg_quangtri2024-25/GCDS.cpp
+++ chondt-hsgqg_quangtri2024-25/GCDS.cpp
@@ -5,16 +5,65 @@ using namespace std;
 #define FOD(i,r,l) for (int i = r; i >= l; --i)
 #define ll long long
 #define ld long double
+#define vi vector<int>
+#define pb push_back
+#define endl '\n'
+
+int a[(int)5e5 + 5];
+ll f[(int)5e5 + 5];
+vi divs[(int)5e5 + 5];
+
+const ll mod = 1e9 + 7;
+ll res = 0;
+
+void process(int n, bool delta) {
+	for (int i : divs[n]) {
+		int j = n / i;
+		if (delta) {
+			res = (res + 1LL * j * f[i]) % mod;
+			f[i] += j;
+		}
+		else {
+			f[i] -= j;
+			res = (res - 1LL * j * f[i] % mod + mod) % mod;
+		}
+	}
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	if(fopen("GCDS.inp", "r")) {
+	if (fopen("GCDS.inp", "r")) {
 		freopen("GCDS.inp", "r", stdin);
 		freopen("GCDS.out", "w", stdout);
 	}
 
-	
+	for (int i = 1; i * i <= 5e5; i++) {
+		divs[i * i].pb(i);
+		for (int j = i * (i + 1); j <= 5e5; j += i) {
+			divs[j].pb(i);
+			divs[j].pb(j / i);
+		}
+	}
+
+	int n, q;
+	cin >> n >> q;
+
+	FOR(i, 1, n)cin >> a[i];
+	FOR(i, 1, n) {
+		process(a[i], 1);
+	}
+
+	cout << res << endl;
+
+	while (q--) {
+		int x, y;
+		cin >> x >> y;
+		process(a[x], 0);
+		a[x] = y;
+		process(a[x], 1);
+		cout << res << endl;
+	}
 }
--- chondt-hsgqg_quangtri2024-25/SCORES.cpp
+++ chondt-hsgqg_quangtri2024-25/SCORES.cpp
@@ -6,15 +6,29 @@ using namespace std;
 #define ll long long
 #define ld long double
 
+ll dp[5005][5005];
+const ll mod = 1e9 + 7;
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	if(fopen("SCORES.inp", "r")) {
+	if (fopen("SCORES.inp", "r")) {
 		freopen("SCORES.inp", "r", stdin);
 		freopen("SCORES.out", "w", stdout);
 	}
+	int t, n;
+	cin >> t;
+	while (t--) {
+		cin >> n;
 
-	
+		memset(dp, 0, sizeof dp);
+		dp[0][1] = 1;
+		FOR(i, 1, n)FOR(j, 1, n) {
+			dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+			dp[i][j] %= mod;
+		}
+		cout << dp[n][n] << endl;
+	}
 }
