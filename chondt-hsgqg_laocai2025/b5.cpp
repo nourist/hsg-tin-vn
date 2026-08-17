cf1978793ba9fca5e6b946844f0fb9e725cd088e
Nourist <hodinhvys@gmail.com>
2026-08-17 21:01:54 +0700

    Update contest: chondt-hsgqg_laocai2025

--- chondt-hsgqg_laocai2025/b5.cpp
+++ chondt-hsgqg_laocai2025/b5.cpp
@@ -25,7 +25,33 @@ using namespace std;
 #define TURN(x, i) ((x) ^ (1LL << (i)))
 #define Name "b5"
 
-bool f[(int)1e5 + 6];
+bool f[(int)1e6 + 6];
+vi nums;
+bool ok[] = { 1, 0, 1, 1, 0, 0 };
+ll a[101][101];
+ll fre[101][101];
+
+unordered_map<ll, bool>mp;
+
+bool check(ll u) {
+	if (u <= 1e6)return f[u];
+
+	if ((u - 1) % 2 == 0) {
+		if (check(u / 2)) {
+			mp[u] = 1;
+			return 1;
+		}
+	}
+
+	if ((u - 1) % 3 == 0) {
+		if (check(u / 3)) {
+			mp[u] = 1;
+			return 1;
+		}
+	}
+
+	return 0;
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -38,10 +64,29 @@ int main() {
 	}
 
 	f[1] = 1;
-	FOR(i, 1, 1e5) {
-		if (i * 2 + 1 <= 1e5)f[i * 2 + 1] = 1;
-		if (i * 3 + 1 <= 1e5)f[i * 3 + 1] = 1;
+	FOR(i, 1, 1e6) {
+		if (!f[i])continue;
+		if (i * 2 + 1 <= 1e6)f[i * 2 + 1] = 1;
+		if (i * 3 + 1 <= 1e6)f[i * 3 + 1] = 1;
 	}
 
-	cout << accumulate(f + 1, f + (int)1e5 + 1, 0LL) << endl;
+	FOR(i, 1, 1e6)if (f[i])nums.pb(i);
+
+	int n;
+	cin >> n;
+	FOR(i, 1, n)FOR(j, 1, n)a[i][j] = nums[(i - 1) * n + j - 1];
+
+	FOR(i, 1, n)FOR(j, 1, n)fre[i][j] = a[i][j];
+	FOR(i, 1, n)FOR(j, 1, n)fre[i][j] += fre[i - 1][j];
+	FOR(i, 1, n)FOR(j, 1, n)fre[i][j] += fre[i][j - 1];
+
+	FOD(l, n, 1) {
+		FOR(i, l, n)FOR(j, l, n) {
+			int sum = fre[i][j] - fre[i - l][j] - fre[i][j - l] + fre[i - l][j - l];
+			if (check(sum)) {
+				cout << l;
+				return 0;
+			}
+		}
+	}
 }
